d98b8ace618816abcb580c3f566c731d72a37884
Nathalia G <r4ng@Killer-Queen.home>
2020-08-01 22:32:25 -0400

    created generatePeople fucntion to fill up a pandas Dataframe with an X amount of employees data

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
 from employee import employee
+from mimesis import Person
+from mimesis.enums import Gender
+from pandas import DataFrame
+from random import randrange
+
+def generatePple(options,plpAmnt):
+
+    person = Person('en')
+    df = DataFrame(columns=["name","lastName","gender","title","status","yrsExp","salary","lastBonus"])
+    print(df)
+
+    while plpAmnt > 0:
+
+        gender = options["GENDER"][randrange(0,len(options["GENDER"]))]
+        title = options["TITLES"][randrange(0,len(options["TITLES"]))]
+        status = options["STATUS"][randrange(0,len(options["STATUS"]))]
+
+        data = {
+           "name": person.name(gender=Gender[gender]),
+           "lastName": person.last_name(gender=Gender[gender]),
+           "gender":gender,
+           "status":status,
+           "title":title,
+           "yrsExp":randrange(0,15)
+        }
+
+        empObj = employee(data)
+        data["salary"] = empObj.salary
+        data["lastBonus"] = empObj.lastBonus
+
+        df = df.append(data,ignore_index=True)
+        
+
+        plpAmnt = plpAmnt - 1
+
+    return df
 
 def main():
-    data = {"name":"Nathalia","lastName":"Calzado","gender":"Female","status":"contractor","yrsExp":4,"title":"System Administrator"}
-    emp = employee(data)
+    options = {"GENDER":["FEMALE", "MALE"],"TITLES":["MANAGER","DEVELOPER","SYSTEM ADMINISTRATOR","RECRUITER"],"STATUS":["CONTRACTOR","EMPLOYEE"]}
+    
+    employees = generatePple(options,25)
 
-    print(emp.salary)
+    print(employees)
+    
 
 if __name__ == "__main__":
     main()
