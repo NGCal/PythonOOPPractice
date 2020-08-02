6254d1cd699c99cf3dc19358bd26088a6c32eaa0
Nathalia G <r4ng@Killer-Queen.home>
2020-08-02 00:47:31 -0400

    Created two functions to generate two plots. The first one for  Title vs Salary, the second one for Salary vs Years of experiece  by title

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from employee import employee
 from mimesis import Person
 from mimesis.enums import Gender
 from pandas import DataFrame
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
 from random import randrange
 
 def generatePple(options,plpAmnt):
 
     person = Person('en')
     df = DataFrame(columns=["name","lastName","gender","title","status","yrsExp","salary","lastBonus"])
-    print(df)
 
     while plpAmnt > 0:
 
@@ -36,12 +37,36 @@ def generatePple(options,plpAmnt):
 
     return df
 
+def generatePlot1(emplDF):
+     
+    emplDF = emplDF.groupby("title")["salary"].mean()
+    graph = emplDF.plot(title = "Tittle vs Salary")
+    graph.set_xlabel("Title")
+    graph.set_ylabel("Salary")
+
+def generatePlot2(emplDF):
+    fig, ax = plt.subplots(figsize=(18,6))
+    #graph = emplDF.groupby("title").plot(x="yrsExp",y="salary",ax=ax)
+
+    for name, group in emplDF.groupby("title"):
+        graph = group.plot(x="yrsExp", y='salary', ax=ax, label=name, title="Salary vs Years of Experience by Title")
+    
+    graph.set_xlabel("Years of Experience")
+    graph.set_ylabel("Salary")
 def main():
     options = {"GENDER":["FEMALE", "MALE"],"TITLES":["MANAGER","DEVELOPER","SYSTEM ADMINISTRATOR","RECRUITER"],"STATUS":["CONTRACTOR","EMPLOYEE"]}
     
     employees = generatePple(options,25)
 
+    employees = employees.sort_values(by=["title","salary"])
     print(employees)
+
+    generatePlot1(employees)
+    generatePlot2(employees)
+
+    plt.show()
+
+
     
 
 if __name__ == "__main__":
